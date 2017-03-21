614538eeae788fb1a35069ffd5d52d833dee9204
Sergij Fartushniy <far2shok@yahoo.com>
2017-03-21 15:12:52 +0200

    added more category

--- EmojiDictionary/DefinitionViewController.swift
+++ EmojiDictionary/DefinitionViewController.swift
@@ -12,6 +12,11 @@ class DefinitionViewController: UIViewController {
     
     @IBOutlet weak var definitionLabel: UILabel!
     
+    @IBOutlet weak var CategoryLabel: UILabel!
+    
+    @IBOutlet weak var BirthYearLabel: UILabel!
+    
+    
         @IBOutlet weak var emojiLabel: UILabel!
     
     var emoji = "No Emoji"
@@ -21,26 +26,38 @@ class DefinitionViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
+
         emojiLabel.text = emoji
         
         if emoji == "😕"{
             definitionLabel.text = "Сумний Смайлик"
+            BirthYearLabel.text = "BirthYear: 1998"
+            CategoryLabel.text = "Category: Смайлики"
         }
         if emoji == "🤘"{
             definitionLabel.text = "СравПес"
+            BirthYearLabel.text = "BirthYear: 1988"
+            CategoryLabel.text = "Category: Руки"
         }
         if emoji == "🦋"{
             definitionLabel.text = "Метелик"
+            BirthYearLabel.text = "BirthYear: 1900"
+            CategoryLabel.text = "Category: Тварини"
         }
         if emoji == "🥐"{
-            definitionLabel.text = "Сумний Смайлик"
+            definitionLabel.text = "Коржик"
+            BirthYearLabel.text = "BirthYear: 1998"
+            CategoryLabel.text = "Category: Пиріжки"
         }
         if emoji == "🏀"{
-            definitionLabel.text = "Сумний Смайлик"
+            definitionLabel.text = "Баскетбол"
+            BirthYearLabel.text = "BirthYear: 1991"
+            CategoryLabel.text = "Category: Речі"
         }
         if emoji == "🏍"{
             definitionLabel.text = "Моцик"
-        }
+            BirthYearLabel.text = "BirthYear: 1990"
+            CategoryLabel.text = "Category: Тачки"        }
         
         }
 
